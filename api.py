9d1a853aad59585349f83d7c7bbc54f7ff4f8924
Garv Thakral <90109498+GarvThakral@users.noreply.github.com>
2025-10-24 15:48:06 +0530

    more endpoints

--- api.py
+++ api.py
@@ -3,13 +3,24 @@ from modules.jdanalyzer import jdExtractor
 from modules.question_generator import generate_questions
 from classes_types_dec.structured_outputs import JDResponse
 from pydantic import BaseModel
-
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import FileResponse
+from modules.text2speech import convert_to_speech
 app = FastAPI()
+
+origins = ["*","http://localhost:3000"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 class ParseJD(BaseModel):
     job_description:str
-    experience:int
 
 @app.post("/parse_jd")
 def parse_jd(params:ParseJD)->JDResponse:
@@ -20,3 +31,13 @@ def generate_question_endpoint(params:ParseJD):
     jd_dict = jdExtractor(params.job_description)
     return generate_questions(str(jd_dict))
 
+class Text2Speech(BaseModel):
+    question_text:str
+    experience:int
+
+@app.post("/generate_audio")
+def generate_audio(params:Text2Speech):
+    audio_filename = convert_to_speech(params.question_text)
+    return FileResponse(
+        path = f"./{audio_filename}"
+    )
